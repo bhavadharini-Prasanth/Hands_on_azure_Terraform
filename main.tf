d577f9cce87e879af5607d7f9b889edce25fa27d
bhavadharini-prasanth <dharini.bhava96@gmail.com>
2025-06-25 12:05:47 +0000

    changes

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 provider "azurerm" {
     features {}
-    skip_provider_registration = true
+    
 }
 resource "azurerm_resource_group" "rg" {
     name = "813-5f7dff99-hands-on-with-terraform-on-azure"
