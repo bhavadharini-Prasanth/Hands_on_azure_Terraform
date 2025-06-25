86fb4de99999b8d8b0abbc08a6ca53a4d837a1dd
bhavadharini-prasanth <dharini.bhava96@gmail.com>
2025-06-25 11:54:41 +0000

    state

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+  azurerm = {
+    source  = "hashicorp/azurerm"
+    version = ">= 3.43.0"
+  }
+}
+}
+provider "azurerm" {
+    features {}
+    skip_provider_registration = true
+}
+resource "azurerm_resource_group" "rg" {
+    name = "813-5f7dff99-hands-on-with-terraform-on-azure"
+    location = "West US"
+}
+module "securestorage" {
+  source  = "app.terraform.io/TrainingAzureTerraform/securestorage/azurerm"
+  version = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "StorageAzure123hands"
+}
